6048e2661671d98c697022a56b2999f79ec60d77
ThaFishDance <chuffman91@gmail.com>
2026-03-24 20:47:50 -0500

    switching to an all droplet deployment

--- server/app/main.py
+++ server/app/main.py
@@ -1,7 +1,6 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import games, cards
 from .services.scryfall import ScryfallService
@@ -15,17 +14,6 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="MTG Commander Tracker", version="2.0.0", lifespan=lifespan, redirect_slashes=False)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "https://thecloudbrew.com",
-        "https://www.thecloudbrew.com",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(games.router, prefix="/api/games", tags=["games"])
 app.include_router(cards.router, prefix="/api/cards", tags=["cards"])
